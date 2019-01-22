2a994f7e6a188aab0c93ad7172520165afb0e946
tiwood <andre.bindewald@live.com>
2019-01-22 22:02:38 +0100

    Adding tests for resource azuread_user (WIP)

---
+++ azuread/resource_user_test.go
@@ -0,0 +1,138 @@
+package azuread
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/terraform-providers/terraform-provider-azuread/azuread/helpers/ar"
+)
+
+func TestAccAzureADUser_basic(t *testing.T) {
+	resourceName := "azuread_user.test"
+	id := strconv.Itoa(rand.Intn(9999))
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckADApplicationDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccADUser_basic(id),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckADUserExists(resourceName),
+					resource.TestCheckResourceAttrSet(resourceName, "user_principal_name"),
+					resource.TestCheckResourceAttr(resourceName, "display_name", fmt.Sprintf("acctestt%s", id)),
+					resource.TestCheckResourceAttr(resourceName, "mail_nickname", fmt.Sprintf("acctestt%s", id)),
+					resource.TestCheckResourceAttr(resourceName, "account_enabled", "true"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccAzureADUser_update(t *testing.T) {
+	resourceName := "azuread_user.test"
+	id := strconv.Itoa(rand.Intn(9999))
+	updatedId := strconv.Itoa(rand.Intn(9999))
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckADUserDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccADUser_basic(id),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckADUserExists(resourceName),
+					resource.TestCheckResourceAttrSet(resourceName, "user_principal_name"),
+					resource.TestCheckResourceAttr(resourceName, "display_name", fmt.Sprintf("acctestt%s", id)),
+					resource.TestCheckResourceAttr(resourceName, "mail_nickname", fmt.Sprintf("acctestt%s", id)),
+					resource.TestCheckResourceAttr(resourceName, "account_enabled", "true"),
+				),
+			},
+			{
+				Config: testAccADUser_basic(updatedId),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckADUserExists(resourceName),
+					resource.TestCheckResourceAttrSet(resourceName, "user_principal_name"),
+					resource.TestCheckResourceAttr(resourceName, "display_name", fmt.Sprintf("acctestt%s", updatedId)),
+					resource.TestCheckResourceAttr(resourceName, "mail_nickname", fmt.Sprintf("acctestt%s", updatedId)),
+					resource.TestCheckResourceAttr(resourceName, "account_enabled", "true"),
+				),
+			},
+		},
+	})
+}
+
+func testCheckADUserExists(name string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[name]
+		if !ok {
+			return fmt.Errorf("Not found: %q", name)
+		}
+
+		client := testAccProvider.Meta().(*ArmClient).usersClient
+		ctx := testAccProvider.Meta().(*ArmClient).StopContext
+		resp, err := client.Get(ctx, rs.Primary.ID)
+
+		if err != nil {
+			if ar.ResponseWasNotFound(resp.Response) {
+				return fmt.Errorf("Bad: Azure AD User %q does not exist", rs.Primary.ID)
+			}
+			return fmt.Errorf("Bad: Get on Azure AD usersClient: %+v", err)
+		}
+
+		return nil
+	}
+}
+
+func testCheckADUserDestroy(s *terraform.State) error {
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "azuread_user" {
+			continue
+		}
+
+		client := testAccProvider.Meta().(*ArmClient).usersClient
+		ctx := testAccProvider.Meta().(*ArmClient).StopContext
+		resp, err := client.Get(ctx, rs.Primary.ID)
+
+		if err != nil {
+			if ar.ResponseWasNotFound(resp.Response) {
+				return nil
+			}
+
+			return err
+		}
+
+		return fmt.Errorf("Azure AD User still exists:\n%#v", resp)
+	}
+
+	return nil
+}
+
+func testAccADUser_basic(id string) string {
+	return fmt.Sprintf(`
+
+data "azuread_domains" "tenant_domain" {
+	tenant_domain_only = true
+}
+
+resource "azuread_user" "test" {
+	user_principal_name = "acctest%s@${data.azuread_domains.tenant_domain.domains.0.domain_name}"
+	display_name = "acctest%s"
+	mail_nickname = "acctest%s"
+	account_enabled = true
+	password = "%s"
+	force_change_password = true
+}
+`, id, id, id, id)
+}
