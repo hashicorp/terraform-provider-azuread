b78e3678019615b4544158542a75fbfc30ae22a5
tiwood <andre.bindewald@live.com>
2019-01-25 21:00:32 +0100

    updated tests to include new attributes.

--- azuread/data_domains_test.go
+++ azuread/data_domains_test.go
@@ -17,6 +17,7 @@ func TestAccDataSourceAzureADDomains_basic(t *testing.T) {
 				Config: `data "azuread_domains" "test" {}`,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet(dataSourceName, "domains.0.domain_name"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "domains.0.authentication_type"),
 					resource.TestCheckResourceAttrSet(dataSourceName, "domains.0.is_default"),
 					resource.TestCheckResourceAttrSet(dataSourceName, "domains.0.is_initial"),
 					resource.TestCheckResourceAttrSet(dataSourceName, "domains.0.is_verified"),
@@ -26,7 +27,7 @@ func TestAccDataSourceAzureADDomains_basic(t *testing.T) {
 	})
 }
 
-func TestAccDataSourceAzureADDomains_tenantDomainOnly(t *testing.T) {
+func TestAccDataSourceAzureADDomains_onlyInitial(t *testing.T) {
 	dataSourceName := "data.azuread_domains.test"
 
 	resource.ParallelTest(t, resource.TestCase{
@@ -35,12 +36,12 @@ func TestAccDataSourceAzureADDomains_tenantDomainOnly(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: `data "azuread_domains" "test" {
-					tenant_domain_only = true
+					only_initial = true
 				}`,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet(dataSourceName, "domains.0.domain_name"),
 					resource.TestCheckResourceAttrSet(dataSourceName, "domains.0.is_default"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "domains.0.is_initial"),
+					resource.TestCheckResourceAttr(dataSourceName, "domains.0.is_initial", "true"),
 					resource.TestCheckResourceAttrSet(dataSourceName, "domains.0.is_verified"),
 				),
 			},
