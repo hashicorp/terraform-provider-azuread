a24497c98ab93a9278fb79c6078fa65a39828557
tiwood <andre.bindewald@live.com>
2019-01-25 21:46:26 +0100

    New func to validate that a given string is a valid email address

--- azuread/helpers/validate/strings.go
+++ azuread/helpers/validate/strings.go
@@ -2,6 +2,7 @@ package validate
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 )
 
@@ -14,6 +15,21 @@ func NoEmptyStrings(i interface{}, k string) ([]string, []error) {
 
 	if strings.TrimSpace(v) == "" {
 		return nil, []error{fmt.Errorf("%q must not be empty", k)}
+	}
+
+	return nil, nil
+}
+
+// StringIsEmailAddress validates that the given string is a valid email address (foo@bar.com)
+func StringIsEmailAddress(i interface{}, k string) ([]string, []error) {
+	v, ok := i.(string)
+	if !ok {
+		return nil, []error{fmt.Errorf("expected type of %q to be string", k)}
+	}
+
+	regExIsEmailAddress := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+	if !regExIsEmailAddress.MatchString(v) {
+		return nil, []error{fmt.Errorf("%q must be in email address format", k)}
 	}
 
 	return nil, nil
