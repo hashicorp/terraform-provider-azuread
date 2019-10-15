319b26284bee7f53650dcfca7cec30839a22005a
Joakim Bakke Hellum <joakim.bakke.hellum@innovasjonnorge.no>
2019-10-15 15:52:30 +0200

    azuread_users: add lookup by mail_nicknames

--- azuread/data_users_test.go
+++ azuread/data_users_test.go
@@ -50,6 +50,27 @@ func TestAccAzureADUsersDataSource_byObjectIds(t *testing.T) {
 	})
 }
 
+func TestAccAzureADUsersDataSource_byMailNicknames(t *testing.T) {
+	dsn := "data.azuread_users.test"
+	id := tf.AccRandTimeInt()
+	password := "p@$$wR2" + acctest.RandStringFromCharSet(7, acctest.CharSetAlphaNum)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAzureADUsersDataSource_byMailNicknames(id, password),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dsn, "user_principal_names.#", "2"),
+					resource.TestCheckResourceAttr(dsn, "object_ids.#", "2"),
+					resource.TestCheckResourceAttr(dsn, "mail_nicknames.#", "2"),
+				),
+			},
+		},
+	})
+}
+
 func testAccAzureADUsersDataSource_byUserPrincipalNames(id int, password string) string {
 	return fmt.Sprintf(`
 %s
@@ -66,6 +87,16 @@ func testAccAzureADUsersDataSource_byObjectIds(id int, password string) string {
 
 data "azuread_users" "test" {
   object_ids = ["${azuread_user.testA.object_id}", "${azuread_user.testB.object_id}"]
+}
+`, testAccADUser_threeUsersABC(id, password))
+}
+
+func testAccAzureADUsersDataSource_byMailNicknames(id int, password string) string {
+	return fmt.Sprintf(`
+%s
+
+data "azuread_users" "test" {
+  mail_nicknames = ["${azuread_user.testA.mail_nickname}", "${azuread_user.testB.mail_nickname}"]
 }
 `, testAccADUser_threeUsersABC(id, password))
 }
