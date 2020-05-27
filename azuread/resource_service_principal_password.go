dab872e0e742857d6c5dbd6fd7f8145371557179
Tom Bamford <tom@bamford.io>
2020-05-27 04:07:47 +0100

    Test for nil service principal password description

--- azuread/resource_service_principal_password.go
+++ azuread/resource_service_principal_password.go
@@ -104,7 +104,10 @@ func resourceServicePrincipalPasswordRead(d *schema.ResourceData, meta interface
 	// value is available in the SDK but isn't returned from the API
 	d.Set("key_id", credential.KeyID)
 	d.Set("service_principal_id", id.ObjectId)
-	d.Set("description", string(*credential.CustomKeyIdentifier))
+
+	if description := credential.CustomKeyIdentifier; description != nil {
+		d.Set("description", string(*description))
+	}
 
 	if endDate := credential.EndDate; endDate != nil {
 		d.Set("end_date", endDate.Format(time.RFC3339))
