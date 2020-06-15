1464e29a2e3d4a8d3c964f577e20c0dfa58008da
Patrick Marabeas <patrick@marabeas.io>
2020-06-15 14:28:27 +1000

    Allow user lookup to gracefully fail; return all user data
    * Add graceful_errors parameter to allow user lookup to gracefully fail.
    This allows only users which were successfully looked up to return. The
    data source will still result in an error if no users were returned.
    
    * Add users parameter - an array of users which mirrors that of the user
    data source. This allows for additional validation of users - such as
    checking whether the account is enabled.
    
    * Documentation updated to include the two new parameters.

--- azuread/data_users.go
+++ azuread/data_users.go
@@ -9,6 +9,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/services/graphrbac/1.6/graphrbac"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 
+	"github.com/terraform-providers/terraform-provider-azuread/azuread/helpers/ar"
 	"github.com/terraform-providers/terraform-provider-azuread/azuread/helpers/graph"
 	"github.com/terraform-providers/terraform-provider-azuread/azuread/helpers/validate"
 )
@@ -23,8 +24,8 @@ func dataUsers() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"object_ids": {
-				Type:          schema.TypeList,
-				Optional:      true,
+				Type:         schema.TypeList,
+				Optional:     true,
 				Computed:      true,
 				ConflictsWith: []string{"user_principal_names"},
 				Elem: &schema.Schema{
@@ -34,8 +35,8 @@ func dataUsers() *schema.Resource {
 			},
 
 			"user_principal_names": {
-				Type:          schema.TypeList,
-				Optional:      true,
+				Type:         schema.TypeList,
+				Optional:     true,
 				Computed:      true,
 				ConflictsWith: []string{"object_ids"},
 				Elem: &schema.Schema{
@@ -54,6 +55,70 @@ func dataUsers() *schema.Resource {
 					ValidateFunc: validate.NoEmptyStrings,
 				},
 			},
+
+			"ignore_missing": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
+
+			"users": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"account_enabled": {
+							Type:     schema.TypeBool,
+							Computed: true,
+						},
+
+						"display_name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+
+						"immutable_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+
+						"mail": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+
+						"mail_nickname": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+
+						"object_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+
+						"onpremises_sam_account_name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+
+						"onpremises_user_principal_name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+
+						"usage_location": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+
+						"user_principal_name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
 		},
 	}
 }
@@ -62,54 +127,83 @@ func dataSourceUsersRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*ArmClient).usersClient
 	ctx := meta.(*ArmClient).StopContext
 
-	var users []graphrbac.User
+	var users []*graphrbac.User
 	expectedCount := 0
 
+	ignoreMissing := d.Get("ignore_missing").(bool)
 	if upns, ok := d.Get("user_principal_names").([]interface{}); ok && len(upns) > 0 {
 		expectedCount = len(upns)
 		for _, v := range upns {
-			resp, err := client.Get(ctx, v.(string))
+			u, err := client.Get(ctx, v.(string))
 			if err != nil {
+				if ignoreMissing && ar.ResponseWasNotFound(u.Response) {
+					break
+				}
 				return fmt.Errorf("Error making Read request on AzureAD User with ID %q: %+v", v.(string), err)
 			}
-
-			users = append(users, resp)
+			users = append(users, &u)
 		}
 	} else if oids, ok := d.Get("object_ids").([]interface{}); ok && len(oids) > 0 {
 		expectedCount = len(oids)
 		for _, v := range oids {
 			u, err := graph.UserGetByObjectId(&client, ctx, v.(string))
 			if err != nil {
+				if ignoreMissing && ar.ResponseWasNotFound(u.Response) {
+					break
+				}
 				return fmt.Errorf("Error finding Azure AD User with object ID %q: %+v", v.(string), err)
 			}
-			users = append(users, *u)
+			users = append(users, u)
 		}
 	} else if mailNicknames, ok := d.Get("mail_nicknames").([]interface{}); ok && len(mailNicknames) > 0 {
 		expectedCount = len(mailNicknames)
 		for _, v := range mailNicknames {
 			u, err := graph.UserGetByMailNickname(&client, ctx, v.(string))
 			if err != nil {
+				if ignoreMissing && ar.ResponseWasNotFound(u.Response) {
+					break
+				}
 				return fmt.Errorf("Error finding Azure AD User with email alias %q: %+v", v.(string), err)
 			}
-			users = append(users, *u)
+			users = append(users, u)
 		}
 	}
 
-	if len(users) != expectedCount {
+	if !ignoreMissing && len(users) != expectedCount {
 		return fmt.Errorf("Unexpected number of users returned (%d != %d)", len(users), expectedCount)
+	}
+
+	if ignoreMissing && len(users) == 0 {
+		return fmt.Errorf("No users were returned")
 	}
 
 	upns := make([]string, 0, len(users))
 	oids := make([]string, 0, len(users))
 	mailNicknames := make([]string, 0, len(users))
+	var userList []map[string]interface{}
 	for _, u := range users {
 		if u.ObjectID == nil || u.UserPrincipalName == nil {
 			return fmt.Errorf("User with nil ObjectId or UPN was found: %v", u)
 		}
 
-		oids = append(oids, *u.ObjectID)
-		upns = append(upns, *u.UserPrincipalName)
-		mailNicknames = append(mailNicknames, *u.MailNickname)
+		if u.ObjectID != nil || u.UserPrincipalName != nil {
+			oids = append(oids, *u.ObjectID)
+			upns = append(upns, *u.UserPrincipalName)
+			mailNicknames = append(mailNicknames, *u.MailNickname)
+
+			user := make(map[string]interface{})
+			user["account_enabled"] = u.AccountEnabled
+			user["display_name"] = u.DisplayName
+			user["immutable_id"] = u.ImmutableID
+			user["mail"] = u.Mail
+			user["mail_nickname"] = u.MailNickname
+			user["object_id"] = u.ObjectID
+			user["onpremises_sam_account_name"] = u.AdditionalProperties["onPremisesSamAccountName"]
+			user["onpremises_user_principal_name"] = u.AdditionalProperties["onPremisesUserPrincipalName"]
+			user["usage_location"] = u.UsageLocation
+			user["user_principal_name"] = u.UserPrincipalName
+			userList = append(userList, user)
+		}
 	}
 
 	h := sha1.New()
@@ -121,5 +215,7 @@ func dataSourceUsersRead(d *schema.ResourceData, meta interface{}) error {
 	d.Set("object_ids", oids)
 	d.Set("user_principal_names", upns)
 	d.Set("mail_nicknames", mailNicknames)
+	d.Set("users", userList)
+
 	return nil
 }
