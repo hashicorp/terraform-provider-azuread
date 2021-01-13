3a19ed0253906781f5a7ae7c371a8d0e7bac0d11
Tom Bamford <tom@bamford.io>
2021-01-13 00:46:12 +0000

    Microsoft Graph support for Domains

--- internal/services/domains/client/client.go
+++ internal/services/domains/client/client.go
@@ -2,19 +2,23 @@ package client
 
 import (
 	"github.com/Azure/azure-sdk-for-go/services/graphrbac/1.6/graphrbac"
+	"github.com/manicminer/hamilton/clients"
 
 	"github.com/hashicorp/terraform-provider-azuread/internal/common"
 )
 
 type Client struct {
 	AadClient *graphrbac.DomainsClient
+	MsClient  *clients.DomainsClient
 }
 
 func NewClient(o *common.ClientOptions) *Client {
 	aadClient := graphrbac.NewDomainsClientWithBaseURI(o.AadGraphEndpoint, o.TenantID)
-	o.ConfigureClient(&aadClient.Client, o.AadGraphAuthorizer)
+	msClient := clients.NewDomainsClient(o.TenantID)
+	o.ConfigureClient(&msClient.BaseClient, &aadClient.Client)
 
 	return &Client{
 		AadClient: &aadClient,
+		MsClient:  msClient,
 	}
 }
