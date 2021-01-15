381ac329bd65ddc3e5cad66298c7af70a9cd6c18
Tom Bamford <tom@bamford.io>
2021-01-15 22:44:32 +0000

    Improve provider acceptance tests

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -2,6 +2,7 @@ package provider
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"testing"
 
@@ -10,6 +11,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/manicminer/hamilton/auth"
+
+	"github.com/hashicorp/terraform-provider-azuread/internal/clients"
 )
 
 func TestProvider(t *testing.T) {
@@ -32,30 +35,39 @@ func TestAccProvider_cliAuth(t *testing.T) {
 
 	// Support only Azure CLI authentication
 	provider.ConfigureContextFunc = func(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
-		builder := &authentication.Builder{
-			TenantID:              d.Get("tenant_id").(string),
-			MetadataHost:          d.Get("metadata_host").(string),
-			Environment:           d.Get("environment").(string),
+		environment, aadEnvironment := environment(d.Get("environment").(string))
+
+		aadBuilder := &authentication.Builder{
+			Environment: aadEnvironment,
+			TenantID:    d.Get("tenant_id").(string),
+			TenantOnly:  true,
+
 			SupportsAzureCliToken: true,
-			TenantOnly:            true,
 		}
 
 		authConfig := &auth.Config{
-			TenantID:            d.Get("tenant_id").(string),
-			ClientID:            d.Get("client_id").(string),
+			Environment: environment,
+			TenantID:    d.Get("tenant_id").(string),
+
 			EnableAzureCliToken: true,
 		}
 
-		return buildClient(ctx, provider, authConfig, builder, "", true)
+		return buildClient(ctx, provider, authConfig, aadBuilder, "", true)
 	}
 
 	d := provider.Configure(ctx, terraform.NewResourceConfigRaw(nil))
 	if d != nil && d.HasError() {
 		t.Fatalf("err: %+v", d)
 	}
+
+	if errs := testCheckProvider(provider); len(errs) > 0 {
+		for _, err := range errs {
+			t.Error(err)
+		}
+	}
 }
 
-func TestAccProvider_servicePrincipalAuth(t *testing.T) {
+func TestAccProvider_clientCertificateAuth(t *testing.T) {
 	if os.Getenv("TF_ACC") == "" {
 		return
 	}
@@ -65,34 +77,116 @@ func TestAccProvider_servicePrincipalAuth(t *testing.T) {
 
 	// Support only Service Principal authentication (certificate or secret)
 	provider.ConfigureContextFunc = func(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
-		builder := &authentication.Builder{
-			ClientID:                 d.Get("client_id").(string),
-			ClientSecret:             d.Get("client_secret").(string),
-			TenantID:                 d.Get("tenant_id").(string),
-			MetadataHost:             d.Get("metadata_host").(string),
-			Environment:              d.Get("environment").(string),
-			ClientCertPassword:       d.Get("client_certificate_password").(string),
-			ClientCertPath:           d.Get("client_certificate_path").(string),
-			SupportsClientCertAuth:   true,
-			SupportsClientSecretAuth: true,
-			TenantOnly:               true,
+		environment, aadEnvironment := environment(d.Get("environment").(string))
+
+		aadBuilder := &authentication.Builder{
+			Environment: aadEnvironment,
+			TenantID:    d.Get("tenant_id").(string),
+			ClientID:    d.Get("client_id").(string),
+			TenantOnly:  true,
+
+			SupportsClientCertAuth: true,
+			ClientCertPassword:     d.Get("client_certificate_password").(string),
+			ClientCertPath:         d.Get("client_certificate_path").(string),
 		}
 
 		authConfig := &auth.Config{
-			TenantID:               d.Get("tenant_id").(string),
-			ClientID:               d.Get("client_id").(string),
-			EnableClientCertAuth:   true,
-			ClientCertPath:         d.Get("client_certificate_path").(string),
-			ClientCertPassword:     d.Get("client_certificate_password").(string),
-			EnableClientSecretAuth: true,
-			ClientSecret:           d.Get("client_secret").(string),
+			Environment: environment,
+			TenantID:    d.Get("tenant_id").(string),
+			ClientID:    d.Get("client_id").(string),
+
+			EnableClientCertAuth: true,
+			ClientCertPath:       d.Get("client_certificate_path").(string),
+			ClientCertPassword:   d.Get("client_certificate_password").(string),
 		}
 
-		return buildClient(ctx, provider, authConfig, builder, "", true)
+		return buildClient(ctx, provider, authConfig, aadBuilder, "", true)
 	}
 
 	d := provider.Configure(ctx, terraform.NewResourceConfigRaw(nil))
 	if d != nil && d.HasError() {
 		t.Fatalf("err: %+v", d)
 	}
+
+	if errs := testCheckProvider(provider); len(errs) > 0 {
+		for _, err := range errs {
+			t.Error(err)
+		}
+	}
+}
+
+func TestAccProvider_clientSecretAuth(t *testing.T) {
+	if os.Getenv("TF_ACC") == "" {
+		return
+	}
+
+	provider := AzureADProvider()
+	ctx := context.Background()
+
+	// Support only Service Principal authentication (certificate or secret)
+	provider.ConfigureContextFunc = func(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+		environment, aadEnvironment := environment(d.Get("environment").(string))
+
+		aadBuilder := &authentication.Builder{
+			Environment: aadEnvironment,
+			TenantID:    d.Get("tenant_id").(string),
+			ClientID:    d.Get("client_id").(string),
+			TenantOnly:  true,
+
+			SupportsClientSecretAuth: true,
+			ClientSecret:             d.Get("client_secret").(string),
+		}
+
+		authConfig := &auth.Config{
+			Environment: environment,
+			TenantID:    d.Get("tenant_id").(string),
+			ClientID:    d.Get("client_id").(string),
+
+			EnableClientSecretAuth: true,
+			ClientSecret:           d.Get("client_secret").(string),
+		}
+
+		return buildClient(ctx, provider, authConfig, aadBuilder, "", true)
+	}
+
+	d := provider.Configure(ctx, terraform.NewResourceConfigRaw(nil))
+	if d != nil && d.HasError() {
+		t.Fatalf("err: %+v", d)
+	}
+
+	if errs := testCheckProvider(provider); len(errs) > 0 {
+		for _, err := range errs {
+			t.Error(err)
+		}
+	}
+}
+
+func testCheckProvider(provider *schema.Provider) (errs []error) {
+	client := provider.Meta().(*clients.Client)
+
+	if client.Environment.AzureADEndpoint == "" {
+		errs = append(errs, fmt.Errorf("AzureADEndpoint was empty in client.Environment"))
+	}
+
+	if client.Environment.MsGraph.Endpoint == "" {
+		errs = append(errs, fmt.Errorf("MsGraphEndpoint was empty in client.Environment"))
+	}
+
+	if client.TenantID == "" {
+		errs = append(errs, fmt.Errorf("client.TenantID was empty"))
+	}
+
+	if client.ClientID == "" {
+		errs = append(errs, fmt.Errorf("client.ClientID was empty"))
+	}
+
+	if client.Claims.TenantId == "" {
+		errs = append(errs, fmt.Errorf("TenantId was not populated in client.Claims"))
+	}
+
+	if client.Claims.ObjectId == "" {
+		errs = append(errs, fmt.Errorf("ObjectId was not populated in client.Claims"))
+	}
+
+	return
 }
