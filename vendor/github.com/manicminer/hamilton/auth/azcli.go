813a063ee4c428e042b674a0e585d6c6246ae920
Tom Bamford <tom@bamford.io>
2021-01-27 23:51:09 +0000

    Bump Hamilton to v0.7.0

--- vendor/github.com/manicminer/hamilton/auth/azcli.go
+++ vendor/github.com/manicminer/hamilton/auth/azcli.go
@@ -4,12 +4,20 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
-	"golang.org/x/oauth2"
 	"os/exec"
 	"regexp"
 	"strings"
 	"time"
+
+	"github.com/hashicorp/go-version"
+	"golang.org/x/oauth2"
+)
+
+const (
+	azureCliMinimumVersion   = "2.0.81"
+	azureCliNextMajorVersion = "3.0.0"
 )
 
 // AzureCliAuthorizer is an Authorizer which supports the Azure CLI.
@@ -37,8 +45,7 @@ func (a AzureCliAuthorizer) Token() (*oauth2.Token, error) {
 	case AadGraph:
 		resourceType = "aad-graph"
 	}
-	cmd := []string{"account", "get-access-token", fmt.Sprintf("--resource-type=%s", resourceType), "--tenant", a.conf.TenantID, "-o=json"}
-	err := jsonUnmarshalAzCmd(&token, cmd...)
+	err := jsonUnmarshalAzCmd(&token, "account", "get-access-token", fmt.Sprintf("--resource-type=%s", resourceType), "--tenant", a.conf.TenantID)
 	if err != nil {
 		return nil, err
 	}
@@ -58,25 +65,20 @@ type AzureCliConfig struct {
 
 // NewAzureCliConfig validates the supplied tenant ID and returns a new AzureCliConfig.
 func NewAzureCliConfig(api Api, tenantId string) (*AzureCliConfig, error) {
-	// check az-cli version
+	var err error
 
-	// check tenant id
-	validTenantId, err := regexp.MatchString("^[a-zA-Z0-9._-]+$", tenantId)
-	if err != nil {
-		return nil, fmt.Errorf("could not parse tenant ID %q: %s", tenantId, err)
+	// check az-cli version
+	if err = checkAzVersion(); err != nil {
+		return nil, err
 	}
 
-	if !validTenantId {
-		var account struct {
-			ID       string `json:"id"`
-			TenantID string `json:"tenantId"`
-		}
-		cmd := []string{"account", "show", "-o=json"}
-		err := jsonUnmarshalAzCmd(&account, cmd...)
-		if err != nil {
-			return nil, fmt.Errorf("obtaining tenant ID: %s", err)
-		}
-		tenantId = account.TenantID
+	// check tenant id
+	tenantId, err = checkTenantId(tenantId)
+	if err != nil {
+		return nil, err
+	}
+	if tenantId == "" {
+		return nil, errors.New("invalid tenantId or unable to determine tenantId")
 	}
 
 	return &AzureCliConfig{Api: api, TenantID: tenantId}, nil
@@ -91,13 +93,78 @@ func (c *AzureCliConfig) TokenSource(ctx context.Context) Authorizer {
 	}
 }
 
+// checkAzVersion tries to determine the version of Azure CLI in the path and checks for a compatible version
+func checkAzVersion() error {
+	var cliVersion *struct {
+		AzureCli          *string      `json:"azure-cli,omitempty"`
+		AzureCliCore      *string      `json:"azure-cli-core,omitempty"`
+		AzureCliTelemetry *string      `json:"azure-cli-telemetry,omitempty"`
+		Extensions        *interface{} `json:"extensions,omitempty"`
+	}
+	err := jsonUnmarshalAzCmd(&cliVersion, "version")
+	if err != nil {
+		return fmt.Errorf("could not parse Azure CLI version: %v", err)
+	}
+
+	if cliVersion.AzureCli == nil {
+		return fmt.Errorf("could not detect Azure CLI version. Please ensure you have installed Azure CLI version %s or newer", azureCliMinimumVersion)
+	}
+
+	actual, err := version.NewVersion(*cliVersion.AzureCli)
+	if err != nil {
+		return fmt.Errorf("could not parse detected Azure CLI version %q: %+v", *cliVersion.AzureCli, err)
+	}
+
+	supported, err := version.NewVersion(azureCliMinimumVersion)
+	if err != nil {
+		return fmt.Errorf("could not parse supported Azure CLI version: %+v", err)
+	}
+
+	nextMajor, err := version.NewVersion(azureCliNextMajorVersion)
+	if err != nil {
+		return fmt.Errorf("could not parse next major Azure CLI version: %+v", err)
+	}
+
+	if nextMajor.LessThanOrEqual(actual) {
+		return fmt.Errorf("unsupported Azure CLI version %q detected, please install a version newer than %s but older than %s", actual, supported, nextMajor)
+	}
+
+	if actual.LessThan(supported) {
+		return fmt.Errorf("unsupported Azure CLI version %q detected, please install version %s or newer and ensure the `az` command is in your path", actual, supported)
+	}
+
+	return nil
+}
+
+// checkTenantId validates the supplied tenant ID, and tries to determine the default tenant if a valid one is not supplied.
+func checkTenantId(tenantId string) (string, error) {
+	validTenantId, err := regexp.MatchString("^[a-zA-Z0-9._-]+$", tenantId)
+	if err != nil {
+		return "", fmt.Errorf("could not parse tenant ID %q: %s", tenantId, err)
+	}
+
+	if !validTenantId {
+		var account struct {
+			ID       string `json:"id"`
+			TenantID string `json:"tenantId"`
+		}
+		err := jsonUnmarshalAzCmd(&account, "account", "show")
+		if err != nil {
+			return "", fmt.Errorf("obtaining tenant ID: %s", err)
+		}
+		tenantId = account.TenantID
+	}
+
+	return tenantId, nil
+}
+
 // jsonUnmarshalAzCmd executes an Azure CLI command and unmarshals the JSON output.
 func jsonUnmarshalAzCmd(i interface{}, arg ...string) error {
 	var stderr bytes.Buffer
 	var stdout bytes.Buffer
 
+	arg = append(arg, "-o=json")
 	cmd := exec.Command("az", arg...)
-
 	cmd.Stderr = &stderr
 	cmd.Stdout = &stdout
 
