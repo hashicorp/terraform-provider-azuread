589ed958de7d353c7f80c02cf3fd0cd618f207b7
Michal Jirman <jirman.michal@gmail.com>
2021-04-30 09:25:00 +0100

    code refactor, bug fixes

--- internal/services/directory_roles/directory_role_data_source.go
+++ internal/services/directory_roles/directory_role_data_source.go
@@ -7,7 +7,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
 	"github.com/hashicorp/terraform-provider-azuread/internal/clients"
-	`github.com/hashicorp/terraform-provider-azuread/internal/tf`
+	"github.com/hashicorp/terraform-provider-azuread/internal/tf"
 	"github.com/hashicorp/terraform-provider-azuread/internal/validate"
 )
 
