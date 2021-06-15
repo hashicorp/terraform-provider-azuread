966d5fae5e360a00c910066ff142072c5c65e9c6
Tom Bamford <tom@bamford.io>
2021-06-15 11:27:40 +0100

    Support SDK nullable strings, non-pointer slice fields and import errors (i.e. not just diagnostics) for use in CustomizeDiff functions

--- internal/tf/marshall.go
+++ internal/tf/marshall.go
@@ -1,19 +1,30 @@
 package tf
 
-func ExpandStringSlicePtr(input []interface{}) *[]string {
+func ExpandStringSlice(input []interface{}) []string {
 	result := make([]string, 0)
 	for _, item := range input {
 		result = append(result, item.(string))
 	}
+	return result
+}
+
+func ExpandStringSlicePtr(input []interface{}) *[]string {
+	result := ExpandStringSlice(input)
 	return &result
+}
+
+func FlattenStringSlice(input []string) []interface{} {
+	result := make([]interface{}, 0)
+	for _, item := range input {
+		result = append(result, item)
+	}
+	return result
 }
 
 func FlattenStringSlicePtr(input *[]string) []interface{} {
 	result := make([]interface{}, 0)
 	if input != nil {
-		for _, item := range *input {
-			result = append(result, item)
-		}
+		result = FlattenStringSlice(*input)
 	}
 	return result
 }
