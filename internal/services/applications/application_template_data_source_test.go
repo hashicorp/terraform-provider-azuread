22f077060ffc4b4209282a9be2b048d8b835ff2e
Tom Bamford <tom@bamford.io>
2021-09-06 23:04:30 +0100

    New data source for application templates

---
+++ internal/services/applications/application_template_data_source_test.go
@@ -0,0 +1,76 @@
+package applications_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/hashicorp/terraform-provider-azuread/internal/acceptance"
+	"github.com/hashicorp/terraform-provider-azuread/internal/acceptance/check"
+)
+
+const (
+	testApplicationTemplateId          = "4601ed45-8ff3-4599-8377-b6649007e876" // Marketo
+	testApplicationTemplateAppRoleId   = "dfd0e7dd-26fb-4b2c-98d2-e444486c1e37" // The app role provided by the template
+	testApplicationTemplateDisplayName = "Marketo"                              // The display name of the template
+)
+
+type ApplicationTemplateDataSource struct{}
+
+func TestAccApplicationTemplateDataSource_byDisplayName(t *testing.T) {
+	data := acceptance.BuildTestData(t, "data.azuread_application_template", "test")
+	r := ApplicationTemplateDataSource{}
+
+	data.DataSourceTest(t, []resource.TestStep{
+		{
+			Config: r.byDisplayName(data),
+			Check:  r.testCheck(data),
+		},
+	})
+}
+
+func TestAccApplicationTemplateDataSource_byTemplateId(t *testing.T) {
+	data := acceptance.BuildTestData(t, "data.azuread_application_template", "test")
+	r := ApplicationTemplateDataSource{}
+
+	data.DataSourceTest(t, []resource.TestStep{
+		{
+			Config: r.byTemplateId(data),
+			Check:  r.testCheck(data),
+		},
+	})
+}
+
+func (ApplicationTemplateDataSource) testCheck(data acceptance.TestData) resource.TestCheckFunc {
+	return resource.ComposeTestCheckFunc(
+		check.That(data.ResourceName).Key("template_id").HasValue(testApplicationTemplateId),
+		check.That(data.ResourceName).Key("display_name").HasValue(testApplicationTemplateDisplayName),
+		check.That(data.ResourceName).Key("categories.#").Exists(),
+		check.That(data.ResourceName).Key("homepage_url").Exists(),
+		check.That(data.ResourceName).Key("logo_url").Exists(),
+		check.That(data.ResourceName).Key("publisher").Exists(),
+		check.That(data.ResourceName).Key("supported_provisioning_types.#").Exists(),
+		check.That(data.ResourceName).Key("supported_single_sign_on_modes.#").Exists(),
+	)
+}
+
+func (ApplicationTemplateDataSource) byTemplateId(_ acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azuread" {}
+
+data "azuread_application_template" "test" {
+  template_id = "%[1]s"
+}
+`, testApplicationTemplateId)
+}
+
+func (ApplicationTemplateDataSource) byDisplayName(_ acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azuread" {}
+
+data "azuread_application_template" "test" {
+  display_name = "%[1]s"
+}
+`, testApplicationTemplateDisplayName)
+}
