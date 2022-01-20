c1ff18090921d90a9231527559931b37ae99f7ac
jackofallops <ste@hashicorp.com>
2022-01-20 17:03:54 +0000

    functional json schema api for consumption in tfpdk

--- internal/tools/schema-api/providerjson/handlers.go
+++ internal/tools/schema-api/providerjson/handlers.go
@@ -3,17 +3,16 @@ package providerjson
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"log"
 	"net/http"
 	"strings"
 )
 
 const (
-	DataSourcesList = "/schema-data/v1/data-sources"
-	ResourcesList   = "/schema-data/v1/resources"
-	DataSourcesPath = "/schema-data/v1/data-sources/"
-	ResourcesPath   = "/schema-data/v1/resources/"
+	DataSourcesList = "/schema-data/v1/data-sources"  // Lists all data sources in the Provider
+	ResourcesList   = "/schema-data/v1/resources"     // Lists all Resources in the Provider
+	DataSourcesPath = "/schema-data/v1/data-sources/" // Gets all schema data for a data source
+	ResourcesPath   = "/schema-data/v1/resources/"    // Gets all schema data for a Resource
 )
 
 func (p ProviderJSON) DataSourcesHandler(w http.ResponseWriter, req *http.Request) {
@@ -21,14 +20,13 @@ func (p ProviderJSON) DataSourcesHandler(w http.ResponseWriter, req *http.Reques
 
 	dsRaw := strings.Split(req.URL.RequestURI(), DataSourcesPath)
 	ds := strings.Split(dsRaw[1], "/")[0]
-	data, err := fromRaw(p.DataSourcesMap[ds])
+	data, err := resourceFromRaw(p.DataSourcesMap[ds])
 	if err != nil {
-		w.Write([]byte(fmt.Sprintf("Could not process schema for %q from provider: %+v", ds, err)))
-	}
-	if len(ds) > 0 {
-		if err := json.NewEncoder(w).Encode(data); err != nil {
-			w.Write([]byte(fmt.Sprintf("Marshall error: %+v", err)))
-		}
+		w.WriteHeader(http.StatusNotFound)
+		log.Println(w.Write([]byte(fmt.Sprintf("[{\"error\": \"Could not process schema for %q from provider: %+v\"}]", ds, err))))
+	} else if err := json.NewEncoder(w).Encode(data); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println(w.Write([]byte(fmt.Sprintf("Marshall error: %+v", err))))
 	}
 }
 
@@ -37,37 +35,26 @@ func (p ProviderJSON) ResourcesHandler(w http.ResponseWriter, req *http.Request)
 
 	dsRaw := strings.Split(req.URL.RequestURI(), ResourcesPath)
 	ds := strings.Split(dsRaw[1], "/")[0]
-	data, err := fromRaw(p.ResourcesMap[ds])
+	data, err := resourceFromRaw(p.ResourcesMap[ds])
 	if err != nil {
-		w.Write([]byte(fmt.Sprintf("Could not process schema for %q from provider: %+v", ds, err)))
-	}
-	if len(ds) > 0 {
-		if err := json.NewEncoder(w).Encode(data); err != nil {
-			w.Write([]byte(fmt.Sprintf("Marshall error: %+v", err)))
-		}
+		w.WriteHeader(http.StatusNotFound)
+		log.Println(w.Write([]byte(fmt.Sprintf("[{\"error\": \"Could not process schema for %q from provider: %+v\"}]", ds, err))))
+	} else if err := json.NewEncoder(w).Encode(data); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println(w.Write([]byte(fmt.Sprintf("Marshall error: %+v", err))))
 	}
 }
 
 func (p *ProviderJSON) ListResources(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	if err := json.NewEncoder(w).Encode(p.Resources()); err != nil {
-		panic(err)
+		log.Println(w.Write([]byte(fmt.Sprintf("Marshall error: %+v", err))))
 	}
 }
 
 func (p *ProviderJSON) ListDataSources(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	if err := json.NewEncoder(w).Encode(p.DataSources()); err != nil {
-		panic(err)
+		log.Println(w.Write([]byte(fmt.Sprintf("Marshall error: %+v", err))))
 	}
-}
-
-func (p *ProviderJSON) DataSources() []terraform.DataSource {
-	s := schema.Provider(*p)
-	return s.DataSources()
-}
-
-func (p *ProviderJSON) Resources() []terraform.ResourceType {
-	s := schema.Provider(*p)
-	return s.Resources()
 }
--- internal/tools/schema-api/providerjson/transform.go
+++ internal/tools/schema-api/providerjson/transform.go
@@ -2,23 +2,43 @@ package providerjson
 
 import (
 	"fmt"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-func fromRaw(input *schema.Resource) (*ResourceJSON, error) {
+func resourceFromRaw(input *schema.Resource) (*ResourceJSON, error) {
 	if input == nil {
 		return nil, fmt.Errorf("resource not found")
 	}
 
-	translatedSchema := make(map[string]SchemaJSON, 0)
+	result := &ResourceJSON{}
+	translatedSchema := make(map[string]SchemaJSON)
 
 	for k, s := range input.Schema {
 		translatedSchema[k] = schemaFromRaw(s)
 	}
+	result.Schema = translatedSchema
 
-	return &ResourceJSON{
-		Schema: translatedSchema,
-	}, nil
+	if input.Timeouts != nil {
+		timeouts := &ResourceTimeoutJSON{}
+		if t := input.Timeouts; t != nil {
+			if t.Create != nil {
+				timeouts.Create = int(t.Create.Minutes())
+			}
+			if t.Read != nil {
+				timeouts.Read = int(t.Read.Minutes())
+			}
+			if t.Delete != nil {
+				timeouts.Delete = int(t.Delete.Minutes())
+			}
+			if t.Update != nil {
+				timeouts.Update = int(t.Update.Minutes())
+			}
+		}
+		result.Timeouts = timeouts
+	}
+
+	return result, nil
 }
 
 func schemaFromRaw(input *schema.Schema) SchemaJSON {
@@ -52,14 +72,18 @@ func decodeConfigMode(input schema.SchemaConfigMode) (out string) {
 
 func decodeElem(input interface{}) interface{} {
 	switch t := input.(type) {
+	case bool:
+		return t
 	case string:
 		return t
 	case int:
 		return t
+	case float32, float64:
+		return t
 	case *schema.Schema:
 		return schemaFromRaw(t)
 	case *schema.Resource:
-		r, _ := fromRaw(t)
+		r, _ := resourceFromRaw(t)
 		return r
 	}
 	return nil
