b18adc286ccd30204ed18c8e88ce553509bfb749
Tom Bamford <tom@bamford.io>
2022-04-12 19:47:46 +0100

    Improve release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
-DIR="$(cd "$(dirname "$0")"/.. && pwd)"
-cd "${DIR}"
+REPO_DIR="$(cd "$(dirname "$0")"/.. && pwd)"
+cd "${REPO_DIR}"
 
 TRUNK="main"
 
 usage() {
-  echo "Usage: $0 -y [-c] [-f]" >&2
+  echo "Usage: $0 -y [-C] [-T] [-f]" >&2
   echo >&2
   echo " -y  Proceed with release. Must be specified." >&2
-  echo " -c  Only prepare the changelog; do not commit, tag or push" >&2
+  echo " -C  Only prepare the changelog; do not commit, tag or push" >&2
+  echo " -T  Skip tests before preparing release" >&2
   echo " -f  Force release prep when \`${TRUNK}\` branch is not checked out" >&2
   echo >&2
 }
 
-while getopts ':ycfh' opt; do
+while getopts ':yCTfh' opt; do
   case "$opt" in
     y)
       GOTIME=1
       ;;
-    c)
+    C)
       NOTAG=1
+      ;;
+    T)
+      NOTEST=1
       ;;
     f)
       FORCE=1
       ;;
-    h)
+    *)
       usage
       exit 1
       ;;
@@ -39,10 +43,10 @@ if [[ "${GOTIME}" != "1" ]]; then
 fi
 
 if [[ "$(uname)" == "Darwin" ]]; then
-  echo "Using BSD sed"
+  echo "(Using BSD sed)"
   SED="sed -E"
 else
-  echo "Using GNU sed"
+  echo "(Using GNU sed)"
   SED="sed -r"
 fi
 
@@ -59,7 +63,19 @@ if [[ "${BRANCH}" != "${TRUNK}" ]]; then
   fi
 fi
 
+if [[ "$(git status --short)" != "" ]]; then
+  echo "Error: working tree is dirty" >&2
+  exit 4
+fi
+
 set -e
+
+if [[ "${NOTEST}" == "1" ]]; then
+  echo "Warning: Skipping tests"
+else
+  echo "Running tests..."
+  ( set -x; TF_ACC= scripts/run-test.sh )
+fi
 
 echo "Preparing changelog for release..."
 
@@ -75,29 +91,33 @@ if [[ "${RELEASE}" == "" ]]; then
   exit 3
 fi
 
+# Ensure latest changes are checked out
+( set -x; git pull --rebase origin "${TRUNK}" )
+
 # Replace [GH-nnnn] references with issue links
-$SED -i.bak "s/\[GH-([0-9]+)\]/\(\[#\1\]\(${PROVIDER_URL}\/\1\)\)/g" CHANGELOG.md
+( set -x; $SED -i.bak "s/\[GH-([0-9]+)\]/\(\[#\1\]\(${PROVIDER_URL}\/\1\)\)/g" CHANGELOG.md )
 
 # Set the date for the latest release
-$SED -i.bak "s/^(## v?[0-9.]+) \(Unreleased\)/\1 (${DATE})/i" CHANGELOG.md
+( set -x; $SED -i.bak "s/^(## v?[0-9.]+) \(Unreleased\)/\1 (${DATE})/i" CHANGELOG.md )
 
 rm CHANGELOG.md.bak
 
-if [[ "${NOTAG}" == 1 ]]; then
-  echo "Skipping commit, tag and push."
+if [[ "${NOTAG}" == "1" ]]; then
+  echo "Warning: Skipping commit, tag and push."
   exit 0
 fi
 
+echo "Committing changelog..."
+(
+  set -x
+  git commit CHANGELOG.md -m v"${RELEASE}"
+  git push origin "${BRANCH}"
+)
+
 echo "Releasing v${RELEASE}..."
 
-# Commit the changelog
-git commit CHANGELOG.md -m "v${RELEASE}"
-
-# Push
-git push origin "${BRANCH}"
-
-# Tag
-git tag v"${RELEASE}"
-
-# Push the tag
-git push origin "v${RELEASE}"
+(
+  set -x
+  git tag v"${RELEASE}"
+  git push origin v"${RELEASE}"
+)
