62974a97c56a3a07fbbd4854dfd1d80796feca66
Theo Andresier <49570057+Threpio@users.noreply.github.com>
2022-08-08 13:45:39 +0300

    Import fmt and tests

---
+++ internal/services/directoryobjects/principal_type_data_source_test.go
@@ -0,0 +1,114 @@
+package directoryobjects_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/hashicorp/terraform-provider-azuread/internal/acceptance"
+	"github.com/hashicorp/terraform-provider-azuread/internal/acceptance/check"
+)
+
+type PrincipalTypeDataSource struct{}
+
+func TestAccPrincipalTypeDataSource_groupByObjectId(t *testing.T) {
+	data := acceptance.BuildTestData(t, "data.azuread_principal_type", "test")
+
+	data.DataSourceTest(t, []resource.TestStep{
+		{
+			Config: PrincipalTypeDataSource{}.objectTypeFromGroup(data),
+			Check: resource.ComposeTestCheckFunc(
+				check.That(data.ResourceName).Key("type").HasValue("Group"),
+			),
+		},
+	})
+}
+
+func TestAccPrincipalTypeDataSource_userByObjectId(t *testing.T) {
+	data := acceptance.BuildTestData(t, "data.azuread_principal_type", "test")
+
+	data.DataSourceTest(t, []resource.TestStep{
+		{
+			Config: PrincipalTypeDataSource{}.objectTypeFromGroup(data),
+			Check: resource.ComposeTestCheckFunc(
+				check.That(data.ResourceName).Key("type").HasValue("User"),
+			),
+		},
+	})
+}
+
+func TestAccPrincipalTypeDataSource_servicePrincipalByObjectId(t *testing.T) {
+	data := acceptance.BuildTestData(t, "data.azuread_principal_type", "test")
+
+	data.DataSourceTest(t, []resource.TestStep{
+		{
+			Config: PrincipalTypeDataSource{}.objectTypeFromServicePrincipal(data),
+			Check: resource.ComposeTestCheckFunc(
+				check.That(data.ResourceName).Key("type").HasValue("ServicePrincipal"),
+			),
+		},
+	})
+}
+
+func (PrincipalTypeDataSource) basicGroup(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+resource "azuread_group" "test" {
+  display_name = "acctestGroup-%d"
+}
+`, data.RandomInteger)
+}
+
+func (PrincipalTypeDataSource) basicUser(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azuread" {}
+
+data "azuread_domains" "test" {
+  only_initial = true
+}
+
+resource "azuread_user" "test" {
+  user_principal_name = "acctestUser'%[1]d@${data.azuread_domains.test.domains.0.domain_name}"
+  display_name        = "acctestUser-%[1]d"
+  password            = "%[2]s"
+}
+`, data.RandomInteger, data.RandomPassword)
+}
+
+func (PrincipalTypeDataSource) basicServicePrincipal(data acceptance.TestData) string {
+	return `
+resource "azuread_service_principal" "test" {
+  display_names = []
+}
+`
+}
+
+func (PrincipalTypeDataSource) objectTypeFromGroup(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%[1]s
+
+data "azuread_principal_type" "test" {
+  object_id = "azuread_group.test.object_id"
+}
+`, PrincipalTypeDataSource{}.basicGroup(data))
+}
+
+func (PrincipalTypeDataSource) objectTypeFromUser(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%[1]s
+
+data "azuread_principal_type" "test" {
+  object_id = "azuread_user.test.object_id"
+}
+`, PrincipalTypeDataSource{}.basicUser(data))
+}
+
+func (PrincipalTypeDataSource) objectTypeFromServicePrincipal(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%[1]s
+
+data "azuread_principal_type" "test" {
+  object_id = "azuread_service_principal.test.object_id"
+}
+`, PrincipalTypeDataSource{}.basicServicePrincipal(data))
+}
