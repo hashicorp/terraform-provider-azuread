cbcac9ce2d37c756c73bcdac4fac11a0e358369d
Marc Gomez <marcgomez@microsoft.com>
2022-12-13 14:15:22 +0100

    Add debug support

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"flag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: Provider,
-	})
+	var debugMode bool
+
+	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	if debugMode {
+		plugin.Serve(&plugin.ServeOpts{
+			ProviderFunc: Provider,
+			ProviderAddr: "registry.terraform.io/hashicorp/azuread",
+			Debug:        true,
+		})
+	} else {
+		plugin.Serve(&plugin.ServeOpts{
+			ProviderFunc: Provider,
+		})
+	}
 }
