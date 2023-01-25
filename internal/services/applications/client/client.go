48169180a16d1198382bb4a1234a6ee486d68c55
Mark Dordoy <MarkDordoy@users.noreply.github.com>
2023-01-25 09:38:59 +0000

    Update client.go

--- internal/services/applications/client/client.go
+++ internal/services/applications/client/client.go
@@ -19,14 +19,6 @@ func NewClient(o *common.ClientOptions) *Client {
 
 	applicationTemplatesClient := msgraph.NewApplicationTemplatesClient(o.TenantID)
 
-	if o.Environment.MsGraph.Endpoint == environments.MsGraphUSGovL4Endpoint {
-		//Short term fix while we wait for Microsoft to fix an intermittent 504 error causing an applicationTemplate instantiate
-		//call to produce a 504. However MS api doesnt cancel request and you end up creating multiple app registrations and service principals
-		//as the client retries. Bug is not present in the beta API.
-		//Expected fix Feb 2023
-		applicationTemplatesClient.BaseClient.ApiVersion = msgraph.VersionBeta
-	}
-
 	o.ConfigureClient(&applicationTemplatesClient.BaseClient)
 
 	directoryObjectsClient := msgraph.NewDirectoryObjectsClient(o.TenantID)
