267738cf96df8da55848d2c56e0c0626929edfac
kt <kt@katbyte.me>
2023-06-20 19:11:07 -0700

    add copyright

--- examples/application/data.tf
+++ examples/application/data.tf
@@ -1,1 +1,4 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
 data "azuread_client_config" "main" {}
--- examples/application/locals.tf
+++ examples/application/locals.tf
@@ -1,3 +1,6 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
 locals {
   widgets_service_scopes = [for s in azuread_application.widgets_service.api.0.oauth2_permission_scope : "${azuread_application.widgets_service.identifier_uris[0]}/${s.value}"]
 }
