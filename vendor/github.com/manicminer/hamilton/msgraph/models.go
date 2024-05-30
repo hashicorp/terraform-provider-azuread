03e359e31302abf7d76832195694c08167a477af
Tom Bamford <tom@bamford.io>
2024-05-30 20:33:12 +0100

    dependencies: updating to `v0.69.0` of `github.com/manicminer/hamilton`

--- vendor/github.com/manicminer/hamilton/msgraph/models.go
+++ vendor/github.com/manicminer/hamilton/msgraph/models.go
@@ -2242,3 +2242,21 @@ type UserFlowAttribute struct {
 	UserFlowAttributeType *string                    `json:"userFlowAttributeType,omitempty"`
 	DataType              *UserflowAttributeDataType `json:"dataType,omitempty"`
 }
+
+type AttributeSet struct {
+	ID                  *string `json:"id,omitempty"`
+	Description         *string `json:"description,omitempty"`
+	MaxAttributesPerSet *int32  `json:"maxAttributesPerSet,omitempty"`
+}
+
+type CustomSecurityAttributeDefinition struct {
+	AttributeSet            *string `json:"attributeSet,omitempty"`
+	Description             *string `json:"description,omitempty"`
+	ID                      *string `json:"id,omitempty"`
+	IsCollection            *bool   `json:"isCollection,omitempty"`
+	IsSearchable            *bool   `json:"isSearchable,omitempty"`
+	Name                    *string `json:"name,omitempty"`
+	Status                  *string `json:"status,omitempty"`
+	Type                    *string `json:"type,omitempty"`
+	UsePreDefinedValuesOnly *bool   `json:"usePreDefinedValuesOnly,omitempty"`
+}
