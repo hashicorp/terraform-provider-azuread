af5d0e8e065da28eac1508eb0ae96d6b477eebfc
id <happytobi@tscoding.de>
2024-07-11 19:59:02 +0000

    bugfix: azuread_application - terrafmt

--- internal/services/applications/application_password_resource_test.go
+++ internal/services/applications/application_password_resource_test.go
@@ -267,8 +267,9 @@ resource "azuread_application" "test" {
 
 resource "azuread_application_password" "test" {
   application_object_id = azuread_application.test.id
-  display_name = "acctest-application-password-%[2]s"
+  display_name          = "acctest-application-password-%[2]s"
 }
+
 
 `, data.RandomInteger, data.RandomString, r.applicationPassword(data.RandomString, renderPassword))
 }
