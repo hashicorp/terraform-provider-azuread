ab528729f99b50a9a65b3a417a55693fa5067431
Tom Bamford <tom@bamford.io>
2024-09-26 00:54:34 +0100

    synchronization: migrate to typed resource IDs

---
+++ internal/services/synchronization/migrations/synchronization_job_resource.go
@@ -0,0 +1,70 @@
+package migrations
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/hashicorp/go-azure-sdk/microsoft-graph/common-types/stable"
+	"github.com/hashicorp/terraform-provider-azuread/internal/helpers/tf/pluginsdk"
+	"github.com/hashicorp/terraform-provider-azuread/internal/services/synchronization/parse"
+)
+
+func ResourceSynchronizationJobInstanceResourceV0() *pluginsdk.Resource {
+	return &pluginsdk.Resource{
+		Schema: map[string]*pluginsdk.Schema{
+			"service_principal_id": {
+				Type:     pluginsdk.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+
+			"template_id": {
+				Type:     pluginsdk.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+
+			"enabled": {
+				Type:     pluginsdk.TypeBool,
+				Default:  true,
+				Optional: true,
+			},
+
+			"schedule": {
+				Type:     pluginsdk.TypeList,
+				Computed: true,
+				Elem: &pluginsdk.Resource{
+					Schema: map[string]*pluginsdk.Schema{
+						"expiration": {
+							Type:     pluginsdk.TypeString,
+							Computed: true,
+						},
+
+						"interval": {
+							Type:     pluginsdk.TypeString,
+							Computed: true,
+						},
+
+						"state": {
+							Type:     pluginsdk.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func ResourceSynchronizationJobInstanceStateUpgradeV0(_ context.Context, rawState map[string]interface{}, _ interface{}) (map[string]interface{}, error) {
+	log.Println("[DEBUG] Migrating ID from v0 to v1 format")
+	oldId, err := parse.SynchronizationJobID(rawState["id"].(string))
+	if err != nil {
+		return rawState, fmt.Errorf("parsing ID for `azuread_synchronization_job`: %+v", err)
+	}
+
+	newId := stable.NewServicePrincipalIdSynchronizationJobID(oldId.ServicePrincipalId, oldId.JobId)
+	rawState["id"] = newId.ID()
+	return rawState, nil
+}
---
+++ internal/services/synchronization/migrations/synchronization_secret_resource.go
@@ -0,0 +1,70 @@
+package migrations
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/hashicorp/go-azure-sdk/microsoft-graph/common-types/stable"
+	"github.com/hashicorp/terraform-provider-azuread/internal/helpers/tf/pluginsdk"
+	"github.com/hashicorp/terraform-provider-azuread/internal/services/synchronization/parse"
+)
+
+func ResourceSynchronizationSecretInstanceResourceV0() *pluginsdk.Resource {
+	return &pluginsdk.Resource{
+		Schema: map[string]*pluginsdk.Schema{
+			"service_principal_id": {
+				Type:     pluginsdk.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+
+			"template_id": {
+				Type:     pluginsdk.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+
+			"enabled": {
+				Type:     pluginsdk.TypeBool,
+				Default:  true,
+				Optional: true,
+			},
+
+			"schedule": {
+				Type:     pluginsdk.TypeList,
+				Computed: true,
+				Elem: &pluginsdk.Resource{
+					Schema: map[string]*pluginsdk.Schema{
+						"expiration": {
+							Type:     pluginsdk.TypeString,
+							Computed: true,
+						},
+
+						"interval": {
+							Type:     pluginsdk.TypeString,
+							Computed: true,
+						},
+
+						"state": {
+							Type:     pluginsdk.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func ResourceSynchronizationSecretInstanceStateUpgradeV0(_ context.Context, rawState map[string]interface{}, _ interface{}) (map[string]interface{}, error) {
+	log.Println("[DEBUG] Migrating ID from v0 to v1 format")
+	oldId, err := parse.SynchronizationSecretID(rawState["id"].(string))
+	if err != nil {
+		return rawState, fmt.Errorf("parsing ID for `azuread_synchronization_secret`: %+v", err)
+	}
+
+	newId := stable.NewServicePrincipalID(oldId.ServicePrincipalId)
+	rawState["id"] = newId.ID()
+	return rawState, nil
+}
