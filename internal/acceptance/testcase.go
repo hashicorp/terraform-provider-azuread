b6a66ed3f22d33a46a9a82b18bc5ec3d6137e432
jackofallops <ste@hashicorp.com>
2025-03-19 07:24:50 +0100

    add mechanism to allow ForceNew tests where appropriate

--- internal/acceptance/testcase.go
+++ internal/acceptance/testcase.go
@@ -72,6 +72,24 @@ func (td TestData) ResourceTest(t *testing.T, testResource types.TestResource, s
 	td.runAcceptanceTest(t, testCase)
 }
 
+// ResourceTestIgnoreRecreate should be used when checking that a resource should be recreated during a test.
+func (td TestData) ResourceTestIgnoreRecreate(t *testing.T, testResource types.TestResource, steps []TestStep) {
+	// Testing framework as of 1.6.0 no longer auto-refreshes state, so adding it back in here for all steps that update
+	// the config rather than having to modify 1000's of tests individually to add a refresh-only step
+	testCase := resource.TestCase{
+		PreCheck: func() { PreCheck(t) },
+		CheckDestroy: func(s *terraform.State) error {
+			client, err := testclient.Build(td.TenantID)
+			if err != nil {
+				return fmt.Errorf("building client: %+v", err)
+			}
+			return helpers.CheckDestroyedFunc(client, testResource, td.ResourceType, td.ResourceName)(s)
+		},
+		Steps: steps,
+	}
+	td.runAcceptanceTest(t, testCase)
+}
+
 func (td TestData) ResourceTestIgnoreDangling(t *testing.T, _ types.TestResource, steps []TestStep) {
 	testCase := resource.TestCase{
 		PreCheck: func() { PreCheck(t) },
