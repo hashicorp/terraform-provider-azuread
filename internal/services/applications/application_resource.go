75257ea0ad44d9f111fab9f47e52a724a7ab0cf4
Brian Weissler <78747334+brian-sqq@users.noreply.github.com>
2025-11-07 16:55:55 -0500

    Update internal/services/applications/application_resource.go
    Update comment per review.
    
    Co-authored-by: jackofallops <11830746+jackofallops@users.noreply.github.com>

--- internal/services/applications/application_resource.go
+++ internal/services/applications/application_resource.go
@@ -1241,7 +1241,7 @@ func applicationResourceCreate(ctx context.Context, d *pluginsdk.ResourceData, m
 
 	// Attempt to patch the newly created application and set the display name, which will tell us whether it exists yet, then set it back to the desired value.
 	// The SDK handles retries for us here in the event of 404, 429 or 5xx, then returns after giving up.
-	// Our retry function also retries on 409.
+	// Due to an unusual implementation on this service, the retry function also retries on 409. See https://github.com/hashicorp/terraform-provider-azuread/issues/1764#issuecomment-3282278691 for more information.
 	uid, err := uuid.GenerateUUID()
 	if err != nil {
 		return tf.ErrorDiagF(err, "Failed to generate a UUID")
