792a611bfa8116277c54aa67b0758e6e7dffd060
jackofallops <ste@hashicorp.com>
2026-02-02 13:17:26 +0100

    bump go version and vendor in WIP go-azure-sdk

--- vendor/github.com/hashicorp/go-azure-sdk/microsoft-graph/applications/beta/application/model_addkeyrequest.go
+++ vendor/github.com/hashicorp/go-azure-sdk/microsoft-graph/applications/beta/application/model_addkeyrequest.go
@@ -4,7 +4,7 @@ import (
 	"github.com/hashicorp/go-azure-sdk/microsoft-graph/common-types/beta"
 )
 
-// Copyright (c) HashiCorp Inc. All rights reserved.
+// Copyright IBM Corp. 2021, 2025 All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
 type AddKeyRequest struct {
--- vendor/github.com/hashicorp/go-azure-sdk/microsoft-graph/serviceprincipals/stable/synchronizationsecret/version.go
+++ vendor/github.com/hashicorp/go-azure-sdk/microsoft-graph/serviceprincipals/stable/synchronizationsecret/version.go
@@ -1,6 +1,6 @@
 package synchronizationsecret
 
-// Copyright (c) HashiCorp Inc. All rights reserved.
+// Copyright IBM Corp. 2021, 2025 All rights reserved.
 // Licensed under the MIT License. See NOTICE.txt in the project root for license information.
 
 const defaultApiVersion = "v1.0"
