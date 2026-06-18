9d0e404ee1c8ff5be903fbc8b9de960f0043c7e8
anurag-hashicorp <anurag.upadhaya@hashicorp.com>
2026-06-18 16:35:00 +0530

    users: add user_license ID parser and user lock constant

--- internal/services/users/constants.go
+++ internal/services/users/constants.go
@@ -3,6 +3,12 @@
 
 package users
 
+// userResourceName is the lock key used to serialize mutations against a single user object. It is
+// intentionally scoped to the parent user (not to any individual child resource) so that every
+// resource which mutates the same user - such as multiple azuread_user_license resources - serializes
+// against the same key. See tf.LockByName.
+const userResourceName = "azuread_user"
+
 const (
 	AgeGroupAdult    = "Adult"
 	AgeGroupMinor    = "Minor"
