3e58910874e6a373424f0e14cb71e5693eae4fe8
anurag-hashicorp <anurag.upadhaya@hashicorp.com>
2026-06-24 14:25:22 +0530

    Add combination configuration acceptance tests

--- internal/services/policies/authentication_strength_policy_resource_test.go
+++ internal/services/policies/authentication_strength_policy_resource_test.go
@@ -79,6 +79,75 @@ func TestAccAuthenticationStrengthPolicy_update(t *testing.T) {
 	})
 }
 
+func TestAccAuthenticationStrengthPolicy_fido2CombinationConfiguration(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azuread_authentication_strength_policy", "test")
+	r := AuthenticationStrengthPolicyResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.fido2CombinationConfiguration(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("fido2_combination_configuration.0.id").Exists(),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func TestAccAuthenticationStrengthPolicy_x509CombinationConfiguration(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azuread_authentication_strength_policy", "test")
+	r := AuthenticationStrengthPolicyResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.x509CombinationConfiguration(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("x509_certificate_combination_configuration.0.id").Exists(),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func TestAccAuthenticationStrengthPolicy_combinationConfigurationUpdate(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azuread_authentication_strength_policy", "test")
+	r := AuthenticationStrengthPolicyResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.fido2(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+		{
+			Config: r.fido2CombinationConfiguration(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("fido2_combination_configuration.0.id").Exists(),
+			),
+		},
+		data.ImportStep(),
+		{
+			Config: r.fido2CombinationConfigurationUpdated(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+		{
+			Config: r.fido2(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
 func (r AuthenticationStrengthPolicyResource) Exists(ctx context.Context, clients *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
 	client := clients.Policies.AuthenticationStrengthPolicyClient
 
@@ -140,6 +209,79 @@ resource "azuread_authentication_strength_policy" "test" {
     "x509CertificateMultiFactor",
     "x509CertificateSingleFactor",
   ]
+}
+`, data.RandomInteger)
+}
+
+func (AuthenticationStrengthPolicyResource) fido2(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azuread" {}
+
+resource "azuread_authentication_strength_policy" "test" {
+  display_name         = "acctestASP-%[1]d"
+  description          = "test"
+  allowed_combinations = ["fido2"]
+}
+`, data.RandomInteger)
+}
+
+func (AuthenticationStrengthPolicyResource) fido2CombinationConfiguration(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azuread" {}
+
+resource "azuread_authentication_strength_policy" "test" {
+  display_name         = "acctestASP-%[1]d"
+  description          = "test"
+  allowed_combinations = ["fido2"]
+
+  fido2_combination_configuration {
+    allowed_aaguids = [
+      "de1e552d-db1d-4423-a619-566b625cdc84",
+      "90a3ccdf-635c-4729-a248-9b709135078f",
+    ]
+  }
+}
+`, data.RandomInteger)
+}
+
+func (AuthenticationStrengthPolicyResource) fido2CombinationConfigurationUpdated(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azuread" {}
+
+resource "azuread_authentication_strength_policy" "test" {
+  display_name         = "acctestASP-%[1]d"
+  description          = "test"
+  allowed_combinations = ["fido2"]
+
+  fido2_combination_configuration {
+    allowed_aaguids = [
+      "de1e552d-db1d-4423-a619-566b625cdc84",
+    ]
+  }
+}
+`, data.RandomInteger)
+}
+
+func (AuthenticationStrengthPolicyResource) x509CombinationConfiguration(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+provider "azuread" {}
+
+resource "azuread_authentication_strength_policy" "test" {
+  display_name = "acctestASP-%[1]d"
+  description  = "test"
+  allowed_combinations = [
+    "x509CertificateSingleFactor",
+    "x509CertificateMultiFactor",
+  ]
+
+  x509_certificate_combination_configuration {
+    applies_to_combinations = [
+      "x509CertificateSingleFactor",
+      "x509CertificateMultiFactor",
+    ]
+    allowed_issuer_skis = ["9af52a26d8e4bd7d5e8f43e9c7c5e2f4a3b1c0d9"]
+    allowed_policy_oids = ["1.2.3.4.5"]
+  }
 }
 `, data.RandomInteger)
 }
