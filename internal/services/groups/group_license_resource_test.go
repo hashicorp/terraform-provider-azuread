8827c72f52ab10219fc46108b223baaa82aefa6a
anurag-hashicorp <anurag.upadhaya@hashicorp.com>
2026-09-14 16:08:15 +0530

    groups: use renamed AAD_TEST_LICENSE_* env vars in group_license tests

--- internal/services/groups/group_license_resource_test.go
+++ internal/services/groups/group_license_resource_test.go
@@ -23,9 +23,9 @@ import (
 type GroupLicenseResource struct{}
 
 func TestAccGroupLicense_basic(t *testing.T) {
-	skuId := os.Getenv("AZUREAD_TEST_SKU_ID")
+	skuId := os.Getenv("AAD_TEST_LICENSE_SKU_ID")
 	if skuId == "" {
-		t.Skip("AZUREAD_TEST_SKU_ID not set")
+		t.Skip("AAD_TEST_LICENSE_SKU_ID not set")
 	}
 
 	data := acceptance.BuildTestData(t, "azuread_group_license", "test")
@@ -45,9 +45,9 @@ func TestAccGroupLicense_basic(t *testing.T) {
 }
 
 func TestAccGroupLicense_requiresImport(t *testing.T) {
-	skuId := os.Getenv("AZUREAD_TEST_SKU_ID")
+	skuId := os.Getenv("AAD_TEST_LICENSE_SKU_ID")
 	if skuId == "" {
-		t.Skip("AZUREAD_TEST_SKU_ID not set")
+		t.Skip("AAD_TEST_LICENSE_SKU_ID not set")
 	}
 
 	data := acceptance.BuildTestData(t, "azuread_group_license", "test")
@@ -65,13 +65,13 @@ func TestAccGroupLicense_requiresImport(t *testing.T) {
 }
 
 func TestAccGroupLicense_disabledPlans(t *testing.T) {
-	skuId := os.Getenv("AZUREAD_TEST_SKU_ID")
+	skuId := os.Getenv("AAD_TEST_LICENSE_SKU_ID")
 	if skuId == "" {
-		t.Skip("AZUREAD_TEST_SKU_ID not set")
+		t.Skip("AAD_TEST_LICENSE_SKU_ID not set")
 	}
-	disabledPlanId := os.Getenv("AZUREAD_TEST_DISABLED_PLAN_ID")
+	disabledPlanId := os.Getenv("AAD_TEST_LICENSE_DISABLED_PLAN_ID")
 	if disabledPlanId == "" {
-		t.Skip("AZUREAD_TEST_DISABLED_PLAN_ID not set")
+		t.Skip("AAD_TEST_LICENSE_DISABLED_PLAN_ID not set")
 	}
 
 	data := acceptance.BuildTestData(t, "azuread_group_license", "test")
