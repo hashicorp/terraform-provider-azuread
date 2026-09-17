e61391b79f50e326b38135b697532d5828bad2ad
Anurag Upadhaya <anurag.upadhaya@hashicorp.com>
2026-09-17 17:57:02 +0530

    New Resource: `azuread_user_license` (#1879)
    Co-authored-by: Todd Giguere <todd.giguere@ibm.com>

--- internal/provider/services.go
+++ internal/provider/services.go
@@ -29,10 +29,11 @@ func SupportedTypedServices() []sdk.TypedServiceRegistration {
 		applications.Registration{},
 		directoryroles.Registration{},
 		domains.Registration{},
+		groups.Registration{},
 		policies.Registration{},
 		identitygovernance.Registration{},
 		serviceprincipals.Registration{},
-		groups.Registration{},
+		users.Registration{},
 	}
 }
 
